2e7d0b1be7c64b9cf20389c29153e9c468a56fae
filipeKaizer <98619997+filipeKaizer@users.noreply.github.com>
2023-09-19 09:49:45 -0300

    Update MacacoInfinito.hs
    Adição de função principal

--- MacacoInfinito.hs
+++ MacacoInfinito.hs
@@ -1,1 +1,14 @@
 
+module Main where
+
+
+main = do
+  putStrLn ""
+
+
+macacoInfinito :: String -> Int
+macacoInfinito texto = if length texto > 10 then 0 else    --Verifica se o texto é muito grande, parando execução se for maior que 10 caracteres
+  let 
+    x = length texto
+  in 
+    x
