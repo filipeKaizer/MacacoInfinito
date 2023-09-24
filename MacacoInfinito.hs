26947b1508e460dbedffcb31bb3f17e2a51f4491
filipeKaizer <98619997+filipeKaizer@users.noreply.github.com>
2023-09-24 19:44:47 -0300

    Update MacacoInfinito.hs

--- MacacoInfinito.hs
+++ MacacoInfinito.hs
@@ -1,11 +1,38 @@
-module Main where
+import System.Random
 
-main = do
-  putStrLn ""
 
-macacoInfinito :: String -> Int
-macacoInfinito texto = if length texto > 10 then 0 else    --Verifica se o texto é muito grande, parando execução se for maior que 10 caracteres
-  let 
-    x = length texto
-  in 
-    x
+-- Adicionei dessa forma, pois o haskell nao tem laco de repeticao. Entao ele chamara recursivamente enquando nao houver a ocorrencia. 
+recursivo :: String -> Int -> Bool -> IO Int
+recursivo _ tentativa True = return tentativa
+recursivo texto tentativa sucesso = do
+  let len = length texto
+  sorteado <- gerarString len
+
+  if comparar sorteado texto 
+    then
+      recursivo texto tentativa True
+    else 
+      recursivo texto (tentativa + 1) False
+  
+  
+      
+--Isso poderia rer sido feito dentro do proprio recursivo com um if-else. Mas o codigo ficaria mais complexo e recheado de bugs. Achei melhor colocar de forma separada
+comparar :: String -> String -> Bool
+comparar textA textB = textA == textB
+
+--Apos varias tentativas, descobri que o melhor é colocar no formato de saida IO Int, assim o resultado confere com a saida da funcao recursivo
+macacoInfinito :: String -> IO Int
+macacoInfinito palavra = do
+  if length palavra > 10 
+    then do
+      putStrLn "A palavra informada é muito grande."
+      return 0
+    else do
+        putStrLn "O macaco encontrou a sequencia na tentativa: "
+        recursivo palavra 1 False --Entendo que o serto seria concatenar tudo no putStrLn, mas encontrei multiplos problemas com o tipo de saida IO () e IO Int dessa funcao
+
+-- A funcao deve ficar abaixo devido problema nao solucionado. Para desenvolver ela, me baseei no codigo de exemplo da professora.
+gerarString :: Int -> IO String
+gerarString tamanho = do
+gen <- newStdGen
+return $ take tamanho $ randomRs ('a', 'z') gen 
